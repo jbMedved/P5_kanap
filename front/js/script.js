ac2ba14c34c020ec457a3b9d7c732943e57499b2
jbhernin <jbhernin@hotmail.com>
2022-03-15 13:35:08 +0100

    lancement de npm + creation fichiers  .js

---
+++ front/js/script.js
@@ -0,0 +1,12 @@
+// 1- ici on va inscrire les VARIABLES dont on se servira par la suite
+
+
+
+// 2- ici on va inscrire les fonctions dont on servire par la suite
+
+
+
+// 3- ici ce sont les classes que l'on va inscrire pour la suite
+
+
+// et voici le code tant attendu
