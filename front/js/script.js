c7cd8b245363ee09a97636418f9b15178ff1ad90
jbhernin <jbhernin@hotmail.com>
2022-03-16 12:24:42 +0100

    ajout des elements récupérés du back sur l'index

--- front/js/script.js
+++ front/js/script.js
@@ -6,11 +6,41 @@ const backEndProducts = "http://localhost:3000/api/products";
 
 
 // 2- ici on va inscrire les fonctions dont on servire par la suite
+//fonction pour aller taper dans le back
+function askToBack(){
+    fetch (backEndProducts)
+    .then (function(res){
+        if (res.ok){
+            return res.json();
+        }
+    })
+    .then (function produits (products){
+        // pour chaque kanap, on fait un inner.html
+        products.forEach((product, i) => {
+            //on colle in setInterval pour la repetition
+            setTimeout(() => { //pour le moment ca marche avec setTimeout mais pas setInterval
+                // contenu 
+                items.innerHTML = `
+                    <a href =./product.html?id=${product._id}>
+                        <article>
+                            <img src ="${product.imageUrl}" alt="${product.altTxt}">
+                            <h3 class= "productName">${product.name}</h3>
+                            <p class = "productDescription">${product.description}</p>
+                        </article>
+                    </a>
+                `
+            },i * 2500)
+        })
+    })
 
+    .catch(function(err){
+        console.log("souci avec le fetch")
+    });
+};
 
 
 // 3- ici ce sont les classes que l'on va inscrire pour la suite
 
 
 // et voici le code tant attendu
-fetch(backEndProducts);
+items.addEventListener('load',askToBack());
