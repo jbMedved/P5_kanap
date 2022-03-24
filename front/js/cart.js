ce1ed14b983d06a249ad219a9e7459fc725b1008
jbhernin <jbhernin@hotmail.com>
2022-03-24 11:54:43 +0100

    quantité et somme auto OK

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,8 +10,11 @@ const cartOrderFormSubmit = document.querySelector(".cart__order__form__submit")
 
 const backEndProducts = "http://localhost:3000/api/products";
 
-let totalQuantityInTheCart = 0;
-let totalPriceOfTheCart = 0;
+let totalQuantityInTheLine = [];
+let totalQuantityInTheCart=0;
+let totalPriceOfTheCart=0;
+let totalPriceOfTheLine = [];
+
 
 // 2- ici on va inscrire les fonctions dont on servire par la suite
 function askToBack(){
@@ -39,7 +42,7 @@ function askToBack(){
 
             // on va "appeler" le contenu du panier
             cart = getCart();
-            console.log(cart);
+            //console.log(cart);
         
         //fonction du bouton "supprimer"
         function removeFromCart(product){
@@ -47,7 +50,7 @@ function askToBack(){
             cart = cart.filter(p => id != product.id || p.color != product.color);
         }
         // combien de lignes a mon panier?
-        console.log(cart.length);
+        //console.log(cart.length);
         // fort de cette information, on va pouvoir "contruire le visuel du panier"
         let i = 0;
         while (i < cart.length){ 
@@ -101,53 +104,95 @@ function askToBack(){
                 saveCart(cart);                         // on l'enregistre cette nouvelle quantité
                 console.log(kanap);
                 //    console.log(cart[i].quantity);
-                calcul(kanap)
+            //    calcul(kanap)
                 })   
             })
 
             //le bouton "supprimer"
             const deleteItem = document.querySelectorAll(".deleteItem");
-            console.log(deleteItem);
+            //console.log(deleteItem);
             
             deleteItem.forEach((suppButton) => {
-                console.log(suppButton);
+                //console.log(suppButton);
                 const article = suppButton.closest('article'); //on recupere l'id en fonction du contenu le la ligne article
-                console.log(article);
+                //console.log(article);
                 const dataId = article.dataset.id; // on en récupere l'id
                 //console.log(dataId);
                 const dataColor = article.dataset.color; // on en récupere la couleur
                 //console.log(dataColor);
                 suppButton.addEventListener('click',() => {
-                    console.log('clic');
-                    const kanap = cart.find(p => p.id == dataId && p.color == dataColor);   //kanap =tout ce qui est different de l'id ou de la couleur
+                //   console.log('clic');
+                    const kanap = cart.find(p => p.id == dataId && p.color == dataColor);   // kanap = id + couleur du canapé
                     article.remove();
-                    console.log(kanap); 
+                //    console.log(kanap); 
                  //   removeFromCart(kanap);    
                     cart = cart.filter(p => p.id != dataId || p.color != dataColor);                                              // on supprime l'article
                     saveCart(cart); 
                 })
-            })
-            //calcul des quantités items et somme totale
-            function calcul(cart) {
-               totalQuantityInTheCart += cart.quantity
-                totalPriceOfTheCart += (cart.quantity * products[s].price)
-                totalQuantity.innerHTML = totalQuantityInTheCart;
-                totalPrice.innerHTML = totalPriceOfTheCart;
-            }
-            calcul(cart[i])
-            
+            })           
             i++;
         
 
         }
-            
-       
+        const itemQuantity = document.querySelectorAll(".itemQuantity");
+        itemQuantity.forEach((lineItem) => {
+            const article = lineItem.closest('article'); //on recupere l'id en fonction du contenu le la ligne article
+            //console.log(article);
+            const dataId = article.dataset.id; // on en récupere l'id
+            //console.log(dataId);
+            const dataColor = article.dataset.color; // on en récupere la couleur            
+            //console.log(dataColor);
+            //ici, on va trouver la quantité en fonction de l'id ET de la couleur
+            function calcul() {
+                totalQuantityInTheCart = 0;
+                totalPriceOfTheCart = 0;
+                let i = 0;
+                while (cart[i].id != dataId || cart[i].color != dataColor ){
+                    i++;
+                } 
+                let s = 0;
+
+                // on additionne les quantités du panier
+                for (let l1=0; l1< cart.length; l1++){                
+                    totalQuantityInTheCart += Number(cart[l1].quantity);
+                    console.log(totalQuantityInTheCart);
+                }
+                //on multiplie id.quantité * id.prix
+                for (let l2=0; l2< cart.length; l2++){                
+                    let quantityId = cart[l2].id; 
+                    console.log(quantityId);
+                    s = 0
+                    while (products[s]._id != cart[l2].id ){
+                        s++; 
+                    }  
+                    
+                    totalPriceOfTheCart += Number(cart[l2].quantity) * Number(products[s].price)
+                    console.log(cart[l2].quantity);
+                    console.log(products[s].price);
+                    console.log(totalQuantityInTheCart);
+                }
+
+                console.log(totalPriceOfTheCart);
+                
+                totalQuantity.innerHTML = totalQuantityInTheCart;
+                totalPrice.innerHTML = totalPriceOfTheCart;
+            }
+
+            lineItem.addEventListener('input',calcul);
+            const deleteItem = document.querySelector(".deleteItem")
+            console.log(deleteItem);
+            deleteItem.addEventListener('click',calcul);
+            cartItems.addEventListener('load',calcul());
+        })
+
+
     })
 }
-// 3- ici ce sont les classes que l'on va inscrire pour la suite
 
 
 // et voici le code tant attendu
 cartOrderFormSubmit.addEventListener('load',askToBack());
+
+
 
 
